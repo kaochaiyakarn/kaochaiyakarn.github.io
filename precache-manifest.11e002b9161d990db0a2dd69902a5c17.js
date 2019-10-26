1b88bd5d0ec5c3cdcf63b32f26dfa304268676b1
kaochaiyakarn <kaochaiyakarn31@gmail.com>
2019-10-27 00:14:24 +0700

    update web

---
+++ precache-manifest.11e002b9161d990db0a2dd69902a5c17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "11bdaf43b96cd770e72d",
+    "url": "/static/js/main.11bdaf43.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "11bdaf43b96cd770e72d",
+    "url": "/static/css/main.96c82368.chunk.css"
+  },
+  {
+    "revision": "9807a88b3ecd08e8b7ca86a91f6ecb36",
+    "url": "/index.html"
+  }
+];
