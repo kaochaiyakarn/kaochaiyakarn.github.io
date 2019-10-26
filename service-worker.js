4b9f11cea3a65328c0508265c987379dc18a2f42
kaochaiyakarn <kaochaiyakarn31@gmail.com>
2019-10-27 02:17:20 +0700

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.11e002b9161d990db0a2dd69902a5c17.js"
+  "/precache-manifest.7e52ca462fab25a16d4130ee3a39f630.js"
 );
 
 workbox.clientsClaim();
